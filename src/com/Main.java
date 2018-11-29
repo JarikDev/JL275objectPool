0b7b5645c684b6a688107b612b506908245b762e
ivan4000 <sxln1@mail.ru>
2018-11-30 02:37:27 +0300

    JL275 object pool

---
+++ src/com/Main.java
@@ -0,0 +1,65 @@
+package com;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        ObjectPool objectPool=new ObjectPool();
+        PooledObject pooledObject=objectPool.getPooledObject();
+        objectPool.releasePooledObject(pooledObject);
+
+    }
+}
+
+class PooledObject {
+}
+
+class ObjectPool {
+    List<PooledObject> free = new LinkedList<>();
+    List<PooledObject> used = new LinkedList<>();
+
+    PooledObject getPooledObject() {
+        if (free.isEmpty()) {
+            PooledObject pooledObject = new PooledObject();
+            free.add(pooledObject);
+            return pooledObject;
+        } else {
+            PooledObject pooledObject = free.get(0);
+            used.add(pooledObject);
+            free.remove(0);
+            return pooledObject;
+        }
+    }
+
+    public void releasePooledObject(PooledObject pooledObject) {
+        used.remove(pooledObject);
+        free.add(pooledObject);
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
